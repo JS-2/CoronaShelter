f0b7d8c22772ed3194a1581567cc979a448c70b8
JS-2 <kimjea23@naver.com>
2020-09-03 18:41:30 +0900

    initial commit

---
+++ CoronaShelter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaShelter
+//
+//  Created by 김재성 on 2020/09/03.
+//  Copyright © 2020 김재성. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
